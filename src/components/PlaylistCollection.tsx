c2bbe34b74ea00d15ceb486c5650530c98042c33
Austin <austink765@gmail.com>
2025-06-18 18:29:20 -0700

    Added onAdd call

--- src/components/PlaylistCollection.tsx
+++ src/components/PlaylistCollection.tsx
@@ -3,25 +3,25 @@ import PlaylistComponent from "./PlaylistComponent";
 import { PlaylistCollectionProps } from "../types/playlist";
 import "../css/PlaylistCollection.css";
 
+
 const PlaylistCollection: React.FC<PlaylistCollectionProps> = ({
   playlists,
   provider,
   status,
   onRemove,
-}) => {
-  return (
-    <div className="playlist-collection">
-      {playlists?.map((playlist) => (
-        <PlaylistComponent
-          key={playlist.id}
-          {...playlist}
-          source={provider || playlist.source}
-          status={status}
-          onRemove={onRemove}
-        />
-      ))}
-    </div>
-  );
-};
+  onAdd,
+}) => (
+  <div className="playlist-collection">
+    {playlists?.map((pl) => (
+      <PlaylistComponent
+        key={pl.id}
+        {...pl}
+        source={provider || pl.source}
+        onRemove={onRemove}
+        onAdd={onAdd}
+      />
+    ))}
+  </div>
+);
 
 export default PlaylistCollection;
