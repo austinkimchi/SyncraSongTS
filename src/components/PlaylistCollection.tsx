74d7efa090770d6f4a31408770d2156f762ffbf3
Austin Kim <contact@austin.kim>
2025-10-18 13:44:18 -0700

    update: restructure interface

--- src/components/PlaylistCollection.tsx
+++ src/components/PlaylistCollection.tsx
@@ -4,21 +4,21 @@ import { PlaylistCollectionProps } from "../types/playlist";
 
 const PlaylistCollection: React.FC<PlaylistCollectionProps> = ({
   playlists,
-  provider,
   // status,
   onRemove,
   onAdd,
 }) => (
   <div className="grid playlist-collection display-grid md:grid-cols-2 lg:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4">
-    {playlists?.map((pl) => (
-      <PlaylistComponent
+    {playlists?.map((pl) => {
+      return (<PlaylistComponent
         key={pl.id}
-        {...pl}
-        platform={provider || pl.platform}
-        onRemove={onRemove}
+        playlist={pl}
         onAdd={onAdd}
-      />
-    ))}
+        onRemove={onRemove}
+      />)
+    }
+
+    )}
   </div>
 );
 
