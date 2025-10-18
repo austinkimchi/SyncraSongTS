3184370ee1e9b84426753cf6170512984ec23746
Austin Kim <contact@austin.kim>
2025-10-18 13:41:32 -0700

    init/add: simple OAuth, maybe need to change to the login OAuth2.0 instead of musickit.

---
+++ src/handler/appleAPI.ts
@@ -0,0 +1,52 @@
+import { API_FULL_URL, APP_FULL_URL } from "../config";
+declare const MusicKit: any;
+
+interface AppleMusicConfig {
+    developerToken: string;
+    app: {
+        name: string;
+        build: string;
+    };
+}
+
+async function redirectToOAuth(): Promise<void> {
+    try {
+        const fetchDevToken = await fetch(`${API_FULL_URL}/api/apple/devToken`, {
+            method: "GET",
+            mode: "cors",
+        });
+
+        if (!fetchDevToken.ok)
+            return console.error("Failed to get developer token");
+
+        const { developerToken } = await fetchDevToken.json();
+        const musicKitConfig: AppleMusicConfig = {
+            developerToken,
+            app: {
+                name: "SyncraSong",
+                build: "0.0.2"
+            },
+        };
+        await MusicKit.configure(musicKitConfig);
+        const musicInstance = MusicKit.getInstance();
+        await musicInstance.authorize();
+        const musicUserToken = await musicInstance.musicUserToken;
+        if (!musicUserToken)
+            return console.error("Failed to authorize with Apple Music");
+
+        window.location.href = `${APP_FULL_URL}/callback/apple?musicUserToken=${musicUserToken}`;
+    }
+    catch (error) {
+        console.error("Error during Apple Music authorization:", error);
+    }
+}
+
+async function handleCallback(): Promise<void> {
+    window.history.replaceState({}, document.title, "/");
+    return;
+}
+
+export {
+    redirectToOAuth as redirectToAppleOAuth,
+    handleCallback as handleAppleCallback
+}
