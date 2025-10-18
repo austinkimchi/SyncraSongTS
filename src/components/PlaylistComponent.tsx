839a82cbc7ef8e1fc047afd8920be30a38b58069
Austin Kim <contact@austin.kim>
2025-10-17 21:49:18 -0700

    fix: +version +tailwind, onRemove changed to Playlist type,

--- src/components/PlaylistComponent.tsx
+++ src/components/PlaylistComponent.tsx
@@ -2,70 +2,71 @@ import React from "react";
 import { Playlist } from "../types/playlist";
 import PlaceholderNoImage from "../assets/placeholders/300x300-noimage.png";
 import { useDrag, DragSourceMonitor } from "react-dnd";
+import { Platform } from "../types/platform";
 import "../css/PlaylistComponent.css";
+import { state } from "../types/status";
 
 interface PlaylistComponentProps extends Playlist {
   onAdd?: (pl: Playlist) => void;
-  onRemove?: (id: string) => void;
+  onRemove?: (pl: Playlist) => void;
 }
 
 const PlaylistComponent: React.FC<PlaylistComponentProps> = ({
   id,
   name,
   image,
-  trackLength,
+  trackCount,
   description,
   isPublic,
   href,
-  source,
+  platform,
   status,
   onAdd,
   onRemove,
 }) => {
   const [{ isDragging }, drag] = useDrag(() => ({
-    type: status === "pending" ? "DRAG_FROM_PENDING" : "DRAG_FROM_PROVIDER",
-    item: { id, name, image, trackLength, status, source, description },
+    type: status === state.PENDING ? "DRAG_FROM_PENDING" : "DRAG_FROM_PROVIDER",
+    item: { id, name, image, trackCount, status, platform, description },
     collect: (monitor: DragSourceMonitor) => ({
       isDragging: monitor.isDragging(),
     }),
   }));
 
   const handleClick = () => {
-    if (status === "pending" && onRemove) onRemove(id);
-    if (status !== "pending" && onAdd) onAdd({
-      id,
-      name,
-      image,
-      trackLength,
-      description,
-      href,
-      isPublic,
-      source,
-      status,
-    });
+    if (status === state.PENDING && onRemove)
+      onRemove({ id, name, image, trackCount, description, isPublic, href, platform, owner: "", status });
+    // if (status !== state.PENDING && onAdd)
+    //   onAdd({ id, name, image, trackCount, description, isPublic, href, platform, owner: "", status });
   };
 
   return (
     <div
       ref={drag}
-      className={`playlist-component ${source === "spotify" ? "spotify-gradient" : "apple-gradient"
-        }`}
+      className={`playlist-component whitespace-nowrap ${platform === Platform.SPOTIFY ? "spotify-gradient" : "apple-gradient"}`}
       style={{
         opacity: isDragging ? 0.5 : 1,
         cursor: "pointer",
       }}
       onClick={handleClick}
+    // onClick will show detailed view of playlist in future update
     >
-      <img
-        src={image || PlaceholderNoImage}
-        alt={name}
-        className="playlist-image"
-      />
-      <div className="playlist-info">
-        <h3 className="playlist-title">{name}</h3>
-        <p className="playlist-song-count">{trackLength} songs</p>
+      <div>
+        <img
+          src={image || PlaceholderNoImage}
+          alt={name}
+          className="playlist-image aspect-square object-cover"
+        />
+
+
+        <h3 className="whitespace-break-spaces text-pretty text-sm font-medium max-w-full text-black line-clamp-2">
+          {name}{'\n\n'}
+        </h3>
       </div>
+
+      <p className="playlist-song-count">{trackCount} songs</p>
     </div>
+
+
   );
 };
 
