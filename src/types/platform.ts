5cdaba0b83250e078010ba19d801637141d96913
Austin Kim <contact@austin.kim>
2025-10-18 13:40:27 -0700

    add: defined definitions for Platform. Defaults to enum, while importing only used function.

--- src/types/platform.ts
+++ src/types/platform.ts
@@ -1,6 +1,8 @@
 import AppleLogo from "../assets/provider/applemusic.svg";
 import SpotifyLogo from "../assets/provider/spotify.png";
 import SoundCloudLogo from "../assets/provider/SoundCloud.png";
+import { handleSpotifyCallback, redirectToSpotifyOAuth } from "../handler/spotifyAPI";
+import { handleAppleCallback, redirectToAppleOAuth } from "../handler/appleAPI";
 
 enum Platform {
     SPOTIFY = "spotify",
@@ -8,31 +10,57 @@ enum Platform {
     SOUNDCLOUD = "soundcloud"
 }
 
-function getPlatformDisplayName(platform: Platform): string {
-    switch (platform) {
-        case Platform.SPOTIFY:
-            return "Spotify";
-        case Platform.APPLE_MUSIC:
-            return "Apple Music";
-        case Platform.SOUNDCLOUD:
-            return "SoundCloud";
-        default:
-            return "unknown";
-    }
-};
-
-
-function getPlatformLogo(platform: Platform): string {
-    switch (platform) {
-        case Platform.SPOTIFY:
-            return SpotifyLogo;
-        case Platform.APPLE_MUSIC:
-            return AppleLogo;
-        case Platform.SOUNDCLOUD:
-            return SoundCloudLogo;
-        default:
-            return "default_logo.png";
-    }
+interface PlatformInfo {
+    id: Platform;
+    displayName: string;
+    loginLabel: string;
+    logo: string;
+    OAuthFunction?: () => Promise<void>;
+    CallbackFunction?: () => Promise<void>;
+    scopes?: string[];
 }
 
-export { Platform, getPlatformDisplayName, getPlatformLogo };
+const PLATFORMS: Record<Platform, PlatformInfo> = {
+    [Platform.SPOTIFY]: {
+        id: Platform.SPOTIFY,
+        displayName: "Spotify",
+        loginLabel: "Sign in with Spotify",
+        logo: SpotifyLogo,
+        OAuthFunction: redirectToSpotifyOAuth,
+        CallbackFunction: handleSpotifyCallback,
+        scopes: [
+            "user-read-private",
+            "user-read-email"
+        ]
+    },
+    [Platform.APPLE_MUSIC]: {
+        id: Platform.APPLE_MUSIC,
+        displayName: "Apple Music",
+        loginLabel: "Sign in with Apple",
+        logo: AppleLogo,
+        OAuthFunction: redirectToAppleOAuth,
+        CallbackFunction: handleAppleCallback
+    },
+    [Platform.SOUNDCLOUD]: {
+        id: Platform.SOUNDCLOUD,
+        displayName: "SoundCloud",
+        loginLabel: "Sign in with SoundCloud",
+        logo: SoundCloudLogo,
+        OAuthFunction: undefined,
+    },
+};
+
+const getPlatformInfo = (p: Platform): PlatformInfo => PLATFORMS[p];
+const getPlatformLogo = (p: Platform): string => PLATFORMS[p].logo;
+const getPlatformDisplayName = (p: Platform): string => PLATFORMS[p].displayName;
+const getPlatformOAuthFunction = (p: Platform): (() => Promise<void>) | undefined => PLATFORMS[p].OAuthFunction;
+const getPlatformCallbackFunction = (p: Platform): (() => Promise<void>) | undefined => PLATFORMS[p].CallbackFunction;
+
+export default Platform;
+export {
+    getPlatformInfo,
+    getPlatformLogo,
+    getPlatformDisplayName,
+    getPlatformOAuthFunction,
+    getPlatformCallbackFunction
+}
